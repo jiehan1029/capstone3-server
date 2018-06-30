da52615a5ea35f4a7be9b3ad098d8daba384674b
Jie Han <jiehan1029@gmail.com>
2018-06-29 23:52:19 -0700

    move image route to my-wall, enable upload & save path & remove files

--- routes/my-wall/router.js
+++ routes/my-wall/router.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const fs=require('fs');
+const path = require('path');
+
+// bodyParser middleware cannot be used with formidable
 const bodyParser=require('body-parser');
-router.use(bodyParser.json());
+//router.use(bodyParser.json());
+
+// to handle formData and file upload
+const formidable = require('formidable');
 
 const jwtDecode=require('jwt-decode');
 
@@ -27,49 +34,84 @@ router.get('/', (req, res) => {
     });
 });
 
-// POST create new records for a specific ticket
-// request supplies date (string, will be processed), imageUri, comment
-router.post('/ticket/:ticketId',(req,res)=>{
-  const ticketId=req.params.ticketId;
-  const userAuth=req.headers.authorization.substr(7,);
-  const username=jwtDecode(userAuth).user.username;
-  const requiredFields = ['imageUri','ticketName'];
-  for (let i = 0; i < requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`;
-      console.error(message);
-      return res.status(400).json({message:message});
-    }
-  }
+// POST create new record (photo collection) for a specific ticket & date
+router.post('/ticket/:ticketId', function (req, res) {
+  console.log('user request to upload image');
 
-  const dateStr=req.body.date || '2001-01-01';
-  const dateObj=strToDate(dateStr);
-  MyRecords
-    .create({
-      username:username,
-      ticketId:ticketId,
-      ticketName:req.body.ticketName,
-      dateStr:dateStr,
-      date:dateObj,
-      imageUrl:{
-        src:imageSrc || '',
-        comment:req.body.comment || ''
-      }
-    })
-    .then(newRecord=>{
-      console.log(`create new record ${newRecord._id} for ticket ${ticketId} of user ${username}`);
-      res.status(201).json(newRecord);
-    })
-    .catch(err=>{
-      console.error(err);
-      res.status(500).json({message:'Internal Server Error'});
+  // takes submitted file and saves it to a public folder
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+
+  form.parse(req, function(err, fields, files) {
+    // `file` is the name of the <input> field of type `file`
+    let old_path = files.file.path,
+    index = old_path.lastIndexOf('\\') + 1,
+    file_name = old_path.substr(index),
+    front_path = path.join('\\uploads\\', file_name),
+    absolute_path = 'E:\\Thinkful\\capstone3\\capstone3-server\\public' + front_path,
+    access_path='http://localhost:8080'+replaceAll(front_path);
+
+    function replaceAll(str) {
+      return str.replace(/\\/g, '/');
+    };
+
+    //console.log('absolute path:', absolute_path);
+    //console.log('access_path',access_path);
+    
+    fs.readFile(old_path, function(err, data) {
+      fs.writeFile(absolute_path, data, function(err) {
+        fs.unlink(old_path, function(err) {
+          if (err) {
+            res.status(500).json({'success': false});
+          }else{
+            const userAuth=req.headers.authorization.substr(7,);
+            const username=jwtDecode(userAuth).user.username;
+            const dateStr=fields.date || '2001-01-01';
+            const dateObj=strToDate(dateStr);
+
+            // check if already have image associated with current user, ticket and date
+            MyRecords
+            .find({username:username,ticketId:fields.ticketId,dateStr:dateStr})
+            .then(docs=>{
+              if(docs.length===0){
+                MyRecords.create({
+                  username:username,
+                  ticketId:req.params.ticketId,
+                  ticketName:fields.ticketName,
+                  dateStr:dateStr,
+                  date:dateObj,
+                  imageUrl:[{
+                    src:access_path,
+                    comment:fields.comment
+                  }]
+                });
+                console.log('upload image success');
+                res.status(201).json({message:'upload success'});
+              }else{
+                docs[0].imageUrl.push({
+                  src:access_path,
+                  comment:fields.comment
+                });
+                docs[0].save()
+                .then(saved=>{
+                  console.log('upload image success');
+                  res.status(201).json({message:'upload success'});
+                })
+              }
+            })
+            .catch(err=>{
+              console.log(err);
+              res.status(500).json({message:'Internal Server Error'});
+            });
+          }
+        });
+      });
     });
+  });
 });
 
 // PUT update a record of the ticket
-// request.body supplies fields to be updated
-router.put('/ticket/:ticketId/record/:recordId',(req,res)=>{
+router.put('/ticket/:ticketId/record/:recordId',bodyParser,(req,res)=>{
   const userAuth=req.headers.authorization.substr(7,);
   const username=jwtDecode(userAuth).user.username;
   const ticketId=req.params.ticketId;
@@ -96,15 +138,77 @@ router.put('/ticket/:ticketId/record/:recordId',(req,res)=>{
     });
 });
 
-// DELETE a record req.body supplies ticketId
+// DELETE a whole collection of photos for a specific record
 router.delete('/ticket/:ticketId/record/:recordId',(req,res)=>{
-  MyRecords
+  // delete the photo files from storage
+  const filePaths=[];
+  MyRecords.find({_id:req.params.recordId})
+  .then(docs=>{
+    docs.map(doc=>{
+      doc.imageUrl.map(imgObj=>{filePaths.push(imgObj.src);});
+    });
+    return filePaths;
+  })
+  .then(filePaths=>{
+    filePaths.map(currPath=>{
+      let index = currPath.lastIndexOf('/') + 1;
+      let fileName = currPath.substr(index,);
+      let memoryPath='E:\\Thinkful\\capstone3\\capstone3-server\\public\\uploads\\'+fileName;
+      console.log('file to delete: ',fileName);
+      fs.unlink(memoryPath, function(err){
+        if(err) return console.error(err);
+        console.log('delete photo successfully');
+      });
+    }); 
+
+    MyRecords
     .findByIdAndRemove(req.params.recordId)
     .then(() => {
-    	console.log(`Deleted record ${req.params.recordId} from ticket ${req.params.ticketId}`);
-    	res.status(204).end();
-    })
-    .catch(err => res.status(500).json(err.message));
+      console.log(`Deleted record ${req.params.recordId} from ticket ${req.params.ticketId}`);
+      res.status(204).end();
+    });
+  })
+  .catch(err=>{
+    console.error(err);
+    res.status(500).json({message:'Internal Server Error'});
+  });
 });
+
+// DELETE a single photo
+router.delete('/image/:imageId',(req,res)=>{
+  const imageId=req.params.imageId;
+  // delete photo url from the database and files from memory
+  let filePath;
+  MyRecords
+  .find({"imageUrl._id":imageId})
+  .then(docs=>{
+    console.log('found doc containing img: ',docs);
+    let toDelete;
+    for(let i=0;i<docs[0].imageUrl.length;i++){
+      if(docs[0].imageUrl[i]._id=imageId){
+        toDelete=i;
+        filePath=docs[0].imageUrl[i].src;
+        break;
+      }
+    }
+    // delete physical file
+    let index = filePath.lastIndexOf('/') + 1;
+    let fileName = filePath.substr(index,);
+    let memoryPath='E:\\Thinkful\\capstone3\\capstone3-server\\public\\uploads\\'+fileName;  
+    fs.unlink(memoryPath, function(err){
+      if(err) return console.error(err);
+      console.log('delete photo files successfully');
+    });
+    // delete database info
+    docs[0].imageUrl.splice(toDelete,1);
+    docs[0].save();
+    console.log(`delete image ${imageId} from the record`);
+    res.status(204).end();
+  })
+  .catch(err=>{
+    console.error(err);
+    res.status(500).json({message:'Internal Server Error'});
+  })
+})
 
 module.exports = {router};
